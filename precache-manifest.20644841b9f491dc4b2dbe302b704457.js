09a0d558dd1c418a1aae9df7e237abed4b5da02f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 15:30:49 +0000

    Deploy benammann/switcheo-exporter to github.com/benammann/switcheo-exporter.git:gh-pages

---
+++ precache-manifest.20644841b9f491dc4b2dbe302b704457.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7047f108bfa920835f1",
+    "url": "/switcheo-exporter/static/js/main.e7047f10.chunk.js"
+  },
+  {
+    "revision": "c0738a2accad7a4b583b",
+    "url": "/switcheo-exporter/static/js/1.c0738a2a.chunk.js"
+  },
+  {
+    "revision": "0f350e924db8c89d2cd9",
+    "url": "/switcheo-exporter/static/js/runtime~main.0f350e92.js"
+  },
+  {
+    "revision": "73dbd4ed2c2cfd12a49f4f90739be7a8",
+    "url": "/switcheo-exporter/index.html"
+  }
+];
